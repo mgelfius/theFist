449608be79e73ecdf43a9088d6de7fcf37735ec7
Mark Gelfius <mgelfius@purdue.edu>
2018-10-23 00:48:03 -0400

    Basic layout

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+function handleSubmit(ev){
+	ev.preventDefault()
+	const f = ev.target
+	const email = f.email.value
+	const firstName = f.firstName.value	
+	const lastName = f.lastName.value
+	const pass = f.pass.value
+}
